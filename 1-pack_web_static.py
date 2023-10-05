c0c477c2625c3d06832eb226cadeb765e7309e5f
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2023-10-06 00:50:22 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-'''Fabric script to generate .tgz archive'''
-
+""" Fabric script that generates a .tgz archive """
+from fabric.decorators import task
 from fabric.api import local
-from datetime import datetime
-
-from fabric.decorators import runs_once
 
 
-@runs_once
+@task
 def do_pack():
-    '''generates .tgz archive from the contents of the web_static folder'''
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
-
-    if result.failed:
-        return None
-    return path
+    """ generates a .tgz archive from web_static """
+    local("mkdir versions ; tar -cvzf \
+versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static/")
